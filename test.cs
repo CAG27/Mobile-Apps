d633ed0ffc774cd1a2c06e3797b29a830cf863f3
Ashley Graham <charlotte.ashley.graham@gmail.com>
2025-09-05 18:51:41 -0500

    Testing 123

---
+++ test.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strA = "The ";
+            string strB = "sentence is ";
+            string strC = "complete.";
+
+            // Write code below this line
+            Console.WriteLine(strA + strB + strC);
+            // Write code above this line
+        }
+    }
+}
